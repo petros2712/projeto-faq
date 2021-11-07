ac28392805ad4ba804fb93d80599f86b3fe8d515
Petros Andrasy <93747536+petros2712@users.noreply.github.com>
2021-11-06 21:58:09 -0300

    adicionando arquivos do projeto

---
+++ faq.js
@@ -0,0 +1,26 @@
+// BUSCAR OS ITENS DA TELA
+const itensPerguntasERespostas = document.querySelectorAll('.item');
+
+// ENTENDER QUE O ITEM FOI CLICADO
+itensPerguntasERespostas.forEach(function(item){
+    item.addEventListener('click', function(){
+        // VERIFICAR SE A PERGUNTA CLICADA ESTA ATIVA
+        const estaAtiva = item.classList.contains('ativo')
+           
+        // EU PRECISO FECHAR TODAS 
+        itensPerguntasERespostas.forEach(function(item) {
+            item.classList.remove('ativo')
+        })
+
+        // SE A PERGUNTA NAO ESTA ATIVA 
+        if(!estaAtiva) {
+            // ABRIR A RESPOSTA ATUAL
+                item.classList.add('ativo')
+        // SE A PERGUNTA ESTA ATIVA
+        } else {
+            // DESATIVAR (REMOVERIA A CLASSE ATIVO)
+            item.classList.remove('ativo')
+            }
+
+    })
+})
